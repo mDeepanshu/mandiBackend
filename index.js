d4c51dfb65f4bddc48c72f6dc8f628b0a075b091
mDeepanshu <deepanshumalviya8964@gmail.com>
2021-05-13 09:02:18 -0700

    sadf

--- index.js
+++ index.js
@@ -1,52 +1,14 @@
-const app = require("./app");
-const debug = require("debug")("node-angular");
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const normalizePort = (val) => {
-  var port = parseInt(val, 10);
+const app = express();
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+const Formatter = require("./routes/Formatter");
+const PartyConnector = require("./routes/PartyConnector");
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+app.use(bodyParser.urlencoded({ extended: false }));
 
-  return false;
-};
+app.use(Formatter);
+app.use(PartyConnector);
 
-const onError = (error) => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  debug("Listening on " + bind);
-};
-
-const port = normalizePort(process.env.PORT || "3000");
-// app.set("port", port);
-
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+app.listen(3000);
--- routes/Formatter.js
+++ routes/Formatter.js
@@ -1,21 +1,40 @@
-class Formatter {
-    static format(body, statusCode) {
-        switch (statusCode) {
-            case 400:
-                return {
-                    status: 400,
-                    title: "Bad Request",
-                    message: body
-                }
-            case 200:
-                return {
-                    status: 200,
-                    title: "Successful",
-                    message: body
-                }
-        }
-    }
-}
+// class Formatter {
+//     static format(body, statusCode) {
+//         switch (statusCode) {
+//             case 400:
+//                 return {
+//                     status: 400,
+//                     title: "Bad Request",
+//                     message: body
+//                 }
+//             case 200:
+//                 return {
+//                     status: 200,
+//                     title: "Successful",
+//                     message: body
+//                 }
+//         }
+//     }
+// }
 
+// module.exports = Formatter
+const express = require("express");
 
-module.exports = Formatter
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+router.get("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
