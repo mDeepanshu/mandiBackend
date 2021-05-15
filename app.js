d7661aab2f2b2a7dad0000bfa3757c514d8bf762
mDeepanshu <deepanshumalviya8964@gmail.com>
2021-05-15 14:43:35 -0700

    .env file setup

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-
-var partyRouter = require('./routes/PartyConnector');
+const dotenv = require("dotenv");
+dotenv.config();
+var partyRouter = require("./routes/PartyConnector");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.use('/party', partyRouter);
+app.use("/party", partyRouter);
 // mongodb+srv://damon:qwert123@cluster0.qyevd.mongodb.net/anilDeriyaJwellers?retryWrites=true
-// mongodb://localhost:27017/
-
+// mongodb://localhost:27017/mandi
+// mongodb+srv://damon:qwert123@cluster0.qyevd.mongodb.net/test?authSource=admin&replicaSet=atlas-khpg0j-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
 mongoose
-  .connect(
-    "mongodb://localhost:27017/mandi",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(process.env.DB_LINK, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to database!");
   })
@@ -38,4 +39,4 @@ app.use((req, res, next) => {
   next();
 });
 */
-module.exports = app
+module.exports = app;
