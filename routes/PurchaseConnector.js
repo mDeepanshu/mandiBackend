d7790e9cfdbb7173d711d7aa316894cf7a711e65
shreyanshumalviya <shreyanshumalviya@gmail.com>
2021-05-20 05:37:46 +0530

    PurchaseConnector.js.js almost finished

--- routes/PurchaseConnector.js
+++ routes/PurchaseConnector.js
@@ -55,12 +55,65 @@ function validateParams(body) {
 }
 
 /** Get Purchase by bill number */
-const by_bill_no = "by_bill_no";
+const by_bill_no = "/by_bill_no";
 router.get(`/${by_bill_no}`, async (req, res) => {
     const {bill_no} = req.query;
     if (bill_no == null) res.send(Formatter.format("bill_no not found", 400));
     let queryResult = await PurchaseModel.findOne({bill_no: bill_no});
     res.send(Formatter.format(queryResult, 200));
-})
+});
+
+/** Get Purchases by date */
+const by_date = "/by_date";
+router.get(`${by_date}`, async (req, res) => {
+    var {date,to_date, from_date} = req.query;
+    if (date == null&&to_date==null&&from_date==null) {
+        res.send(Formatter.format("date not specified", 400));
+        return;
+    }
+    let filter;
+    if (from_date == null)
+        filter = {date: date};
+    else filter = {
+        date: {
+            $gte: from_date,
+            $lt: to_date
+        }
+    }
+    try {
+        console.log("filter", filter);
+        let queryResult = await PurchaseModel.find(filter);
+        console.log("query", queryResult)
+        res.send(Formatter.format(queryResult, 200))
+    }catch (e){
+        res.send(Formatter.format(e.message,400));
+    }
+
+});
+
+/** Get Purchases by party */
+const by_party = "/by_party";
+router.get(`${by_party}`, async (req, res) => {
+    const {party_name, party_id, limit, page} = req.query;
+    if (party_name == null&&party_id==null) {
+        res.send(Formatter.format("party_name or party_id not specified", 400));
+        return;
+    }
+    let filter;
+    if (party_name != null)
+        filter = {party:party_name};
+    else filter = {
+        partyId:party_id
+    }
+    try {
+        let queryResult = await PurchaseModel.find(filter).limit(parseInt(limit)).skip(parseInt(limit*(page-1)));
+        res.send(Formatter.format(queryResult, 200))
+    }catch (e){
+        res.send(Formatter.format(e.message,400));
+    }
+
+});
+
+
 
 module.exports = router;
