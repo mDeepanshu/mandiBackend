66cd8e2fc03e17ac3b2385f3748c87052011b189
shreyanshumalviya <shreyanshumalviya@gmail.com>
2021-06-16 23:23:26 +0530

    crate_transaction/transactions api added

--- routes/CrateTransactionConnector.js
+++ routes/CrateTransactionConnector.js
@@ -5,6 +5,7 @@ const express = require('express');
 const router = express.Router();
 const Formatter = require('./Formatter')
 const CrateTransactionModel = require('../Models/CrateTransactionSchema');
+const CratePartyModel = require('../Models/CratePartySchema');
 
 
 /** Add new crateTransaction */
@@ -18,11 +19,23 @@ router.post(`/${add_new}`, async (req, res) => {
         return;
     }
     try {
+        let party = await CratePartyModel
+            .updateOne({_id: body.partyId},
+                {$inc: {["type" + body.type]: body.count}});
+        if (party["nModified"] === "0") {
+            res.send(Formatter.format("cannot find party", 400));
+            return;
+        }
+    } catch (e) {
+        res.send(Formatter.format("cannot find party", 400));
+        return;
+    }
+    try {
         let a = new CrateTransactionModel(body);
         await a.save();
-        res.send(Formatter.format("successful added", 200));
+        res.send(Formatter.format("successful added ", 200));
     } catch (e) {
-        res.send(Formatter.format("Failed to Add : " + e.message, 200));
+        res.send(Formatter.format("Failed to Add : " + e.message, 400));
     }
 })
 
@@ -36,12 +49,30 @@ function validateParams(body) {
 
 /** Get Transactions */
 const transactions = "transactions";
-router.get(`/${transactions}`,async (req,res)=>{
-    const {partyId, dd,mm,yyyy, from_date,to_date} = req.body;
-    if(from_date==null||to_date == null){
-        let nextDate = Formatter.nextDate(yyyy,mm,dd);
-        
+router.get(`/${transactions}`, async (req, res) => {
+    let {partyId, dd, mm, yyyy, from_date, to_date} = req.query;
+    let nextDate
+    if (from_date == null || to_date == null) {
+        nextDate = Formatter.nextDate(yyyy, mm, dd);
+        from_date = new Date(yyyy, mm - 1, dd);
+        to_date = new Date(nextDate.yyyy, nextDate.mm - 1, nextDate.dd);
     }
+    let party = await CratePartyModel
+        .findOne({_id: partyId}
+            , {_id: 0, type1: 1, type2: 1, type3: 1});
+    let response = {}
 
+    response.current = party;
+    response[transactions] = await CrateTransactionModel.find(
+        {
+            partyId: partyId,
+            date: {
+                $gte: from_date,
+                $lte: to_date
+            }
+        },
+        {_id: 0, date: 1, type: 1, count: 1});
+    res.send(Formatter.format(response, 200));
 });
+
 module.exports = router;
